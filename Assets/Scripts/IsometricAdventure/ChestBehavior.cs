f3b749c6d245aa15d6a193d87cc91c8c5e2ff913
reatured <61943376+reatured@users.noreply.github.com>
2021-05-26 15:30:45 -0400

    Add Chest Open Animation

---
+++ Assets/Scripts/IsometricAdventure/ChestBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBehavior : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("isOpen", true);
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("isOpen", false);
+
+        }
+    }
+}
